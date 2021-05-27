a70f370e86637277b516d8e5b95230349fcabe88
Nitin Sharma <32795343+NitinSharma649@users.noreply.github.com>
2021-05-27 11:13:31 +0530

    Create dynamic_import_axios.js

---
+++ dynamic_import_axios.js
@@ -0,0 +1,11 @@
+(function (i, s, o, g, r, a, m) {
+    i['axios'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js', 'axios');
